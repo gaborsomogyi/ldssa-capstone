bca823460389b4bef35a35528d654dcc0ff6b31f
Gábor Somogyi <gabor.somogyi@gmail.com>
2018-09-15 22:25:24 +0200

    update pipeline files to xgboost

---
+++ pipeline/custom_transformers.py
@@ -0,0 +1,30 @@
+﻿from sklearn.base import TransformerMixin
+import pandas as pd
+
+class NAEncoder(TransformerMixin):
+    """encodes all NA values as 0, non-NA-s as 1"""
+
+    def __init__(self, columns = []):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for col in self.columns:
+            X[col] = X[col].apply(lambda x: int(pd.notna(x)))
+        return X
+    
+class ColumnDropper(TransformerMixin):
+    """Expects a pandas dataframe, drops the columns specified"""
+
+    def __init__(self, columns = []):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        return X.drop(columns=self.columns)
